ff4052f24a969770aa165f5f0b0736a942efc03a
Wilmer Andres Duque Olaya <44647699+wilmerd1@users.noreply.github.com>
2024-09-30 20:20:24 -0500

    conexión Mongos

--- src/database.js
+++ src/database.js
@@ -1,11 +1,33 @@
-const mongoose = require('mongoose')
+/* const mongoose = require('mongoose')
 
 const {DENTAL_APP_MONGOSDB_HOST, DENTAL_APP_MONGOSDB_DATABASE } = process.env;
 const MONGODB_URI = `mongodb://${DENTAL_APP_MONGOSDB_HOST}/${DENTAL_APP_MONGOSDB_DATABASE}`;
+
+
+const uri = "mongodb+srv://zaux:<db_password>@cluster0.capmv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true
 })
 .then(db => console.log('Database is connected'))
-.catch(err => console.log(err));
+.catch(err => console.log(err)); */
+
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb+srv://zaux:0D7Kxmz6tOZo9xpg@cluster0.capmv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Asegúrate de reemplazar <db_password> con tu contraseña real
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Conexión a MongoDB establecida');
+})
+.catch((error) => {
+    console.error('Error al conectar a MongoDB:', error);
+});
+
+module.exports = mongoose;
